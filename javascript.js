89ad2138a647f9ab8b3a01d50305c26e597d5f70
Oskar Gaweł <oskargawel@interia.pl>
2020-05-20 12:07:33 +0200

    change in js file

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
 const name ='Oskar'
 const age ='25'
 
-console.log(`Cześć,nazywam się ${name}. Mam ${age} lat i uczę się frontendu.`)
-alert('LIBERUM VETO!')
+console.log(`Cześć,nazywam się ${name}. Mam ${age} lat i uczę się frontendu-ponieważ frontend to sztuka!`)
