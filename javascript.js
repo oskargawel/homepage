4f2a920aa6d73c3d8cb3c0cd4b8b7e950f9b7d56
Oskar Gaweł <oskargawel@interia.pl>
2020-05-28 10:13:40 +0200

    addition margins

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-const name ='Oskar'
-const age ='25'
+const name = "Oskar";
+const age = "25";
 
-console.log(`Cześć,nazywam się ${name}. Mam ${age} lat i uczę się frontendu-ponieważ frontend to sztuka!`)
+console.log(
+  `Cześć,nazywam się ${name}. Mam ${age} lat i uczę się frontendu-ponieważ frontend to sztuka!`
+);
