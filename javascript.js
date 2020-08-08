da1f0178381b3e69d5ddbb40b2ae95bde3c94330
Oskar Gaweł <oskargawel@interia.pl>
2020-08-08 13:11:01 +0200

    couple changes

--- javascript.js
+++ javascript.js
@@ -4,3 +4,17 @@ const age = "25";
 console.log(
   `Cześć,nazywam się ${name}. Mam ${age} lat i uczę się frontendu-ponieważ frontend to sztuka!`
 );
+const emptyParagraph = document.querySelector('.main__paragraph--js');
+emptyParagraph.innerHTML = 'KŁEBA OMBRE';
+
+const whyDoThatToMe = {
+  name: 'ULĄG',
+  numberOfHands: 5,
+  numberOfHeads: 1,
+  numberOfLegs: 30,
+  numberOfFrustrations: 100,
+}
+
+console.log(whyDoThatToMe)
+
+whyDoThatToMe.numberOfHands = 1
